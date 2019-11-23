c1b0710e0d4da307700f66f19d4bdef43fd717ff
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2019-11-23 09:52:59 +0530

    select queries

--- mysql/README.md
+++ mysql/README.md
@@ -1,3 +1,4 @@
 # Examples
 * [Create DB and User](create-db-and-user.sql)
 * [Practice Tables](practice-tables.sql)
+* [Select Queries](select-queries.sql)
---
+++ mysql/select-queries.sql
@@ -0,0 +1,8 @@
+use practice;
+
+select * from emp;
+select * from dept;
+select * from salgrade;
+select count(*) from emp;
+select count(*) from dept;
+select count(*) from salgrade;
